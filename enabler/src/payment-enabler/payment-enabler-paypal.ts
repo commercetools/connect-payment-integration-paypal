81e545a7bf1054c6cdd06223b037dfb82f1a1eb6
Johnson Ogwuru <johnson.ogwuru@commercetools.com>
2024-02-16 12:42:06 +0100

    chore(paypal-connector): rename instances of mock to paypal (#5)

--- enabler/src/payment-enabler/payment-enabler-paypal.ts
+++ enabler/src/payment-enabler/payment-enabler-paypal.ts
@@ -11,11 +11,11 @@ declare global {
   }
 }
 
-export class MockPaymentEnabler implements PaymentEnabler {
+export class PaypalPaymentEnabler implements PaymentEnabler {
   setupData: Promise<{ baseOptions: BaseOptions }>;
 
   private constructor(options: EnablerOptions) {
-    this.setupData = MockPaymentEnabler._Setup(options);
+    this.setupData = PaypalPaymentEnabler._Setup(options);
   }
 
   private static _Setup = async (options: EnablerOptions): Promise<{ baseOptions: BaseOptions }> => {
