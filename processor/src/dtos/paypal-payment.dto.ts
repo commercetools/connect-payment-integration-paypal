5e0269c62bab16eb79b705b29e73f7c3bcab0377
johnsonogwuru <johnson.ogwuru@commercetools.com>
2024-02-22 15:01:36 +0100

    wip: save point

--- processor/src/dtos/paypal-payment.dto.ts
+++ processor/src/dtos/paypal-payment.dto.ts
@@ -1,18 +1,13 @@
 import { Static, Type } from '@sinclair/typebox';
 
-export const CardPaymentMethodSchema = Type.Object({
+export const PaypalPaymentMethodSchema = Type.Object({
   // TODO: Remove the fields according to the payment provider solution,
   //  Strongly recommend not to process PAN data to Connectors.
-  type: Type.Literal('card'),
-  cardNumber: Type.String(),
-  expiryMonth: Type.Number(),
-  expiryYear: Type.Number(),
-  cvc: Type.Number(),
-  holderName: Type.Optional(Type.String()),
+  type: Type.Literal('paypal'),
 });
 
 export const PaymentRequestSchema = Type.Object({
-  paymentMethod: Type.Composite([CardPaymentMethodSchema]),
+  paymentMethod: Type.Composite([PaypalPaymentMethodSchema]),
 });
 
 export enum PaymentOutcome {
