d60654af41bde8f1ef84f6bd8e04653d47bae0da
johnsonogwuru <johnson.ogwuru@commercetools.com>
2024-03-01 14:59:43 +0100

    feat(connectors): Implement notification endpoint

---
+++ processor/src/services/converters/notification.converter.ts
@@ -0,0 +1,65 @@
+import { TransactionData, UpdatePayment } from '@commercetools/connect-payments-sdk';
+import { COCOTransactionStates, COCOTransactionTypes, NotificationEventType } from '../types/paypal-payment.type';
+import { Money } from '@commercetools/platform-sdk';
+import { NotificationPayloadDTO, NotificationResourceDTO } from '../../dtos/paypal-payment.dto';
+
+export class NotificationConverter {
+  public convert(item: NotificationPayloadDTO): UpdatePayment {
+    return {
+      id: item.resource.invoice_id,
+      transaction: this.populateTransaction(item),
+    };
+  }
+
+  private populateTransaction(item: NotificationPayloadDTO): TransactionData {
+    switch (item.event_type) {
+      case NotificationEventType.PAYMENT_CAPTURE_COMPLETED:
+        return {
+          type: COCOTransactionTypes.CHARGE,
+          state: COCOTransactionStates.SUCCESS,
+          amount: this.convertAmount(item.resource),
+          interactionId: item.resource.id,
+        };
+      case NotificationEventType.PAYMENT_CAPTURE_DECLINED:
+        return {
+          type: COCOTransactionTypes.CHARGE,
+          state: COCOTransactionStates.FAILURE,
+          amount: this.convertAmount(item.resource),
+          interactionId: item.resource.id,
+        };
+      case NotificationEventType.PAYMENT_CAPTURE_REFUNDED:
+        return {
+          type: COCOTransactionTypes.REFUND,
+          state: COCOTransactionStates.SUCCESS,
+          amount: this.convertAmount(item.resource),
+          interactionId: item.resource.id,
+        };
+      case NotificationEventType.PAYMENT_CAPTURE_REVERSED:
+        return {
+          type: COCOTransactionTypes.CHARGE,
+          state: COCOTransactionStates.SUCCESS,
+          amount: this.convertAmount(item.resource),
+          interactionId: item.resource.id,
+        };
+      default:
+        // TODO: improve this error handling
+        throw new Error('Unsupported');
+    }
+  }
+
+  private convertAmount(item: NotificationResourceDTO): Money {
+    return {
+      centAmount: this.parseStringAmountToCentAmount(item.amount.total),
+      currencyCode: item.amount.currency,
+    };
+  }
+
+  private parseStringAmountToCentAmount(amount: string): number {
+    const [units, cents] = amount.split('.').map((part) => parseInt(part, 10));
+    if (isNaN(units) || isNaN(cents)) {
+      throw new Error(`Invalid amount format: ${amount}`);
+    }
+
+    return units * 100 + cents;
+  }
+}
